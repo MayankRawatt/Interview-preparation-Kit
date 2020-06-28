bee95962c10fa4f190e8a635657305e6488d9d20
MayankRawatt <63491892+MayankRawatt@users.noreply.github.com>
2020-06-28 14:18:51 +0530

    initial commit

---
+++ greedy algo/dictionary/two string.py
@@ -0,0 +1,14 @@
+import sys
+
+def twoStrings(s1, s2):
+    if len(set(s1) & set(s2)) > 0:
+        return 'YES'
+    else:
+        return 'NO'
+
+q = int(input().strip())
+for a0 in range(q):
+    s1 = input().strip()
+    s2 = input().strip()
+    result = twoStrings(s1, s2)
+    print(result)
